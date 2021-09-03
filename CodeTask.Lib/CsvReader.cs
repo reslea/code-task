e49af4beae923073fe06ab29abff012651aebf56
Reslea <rmidion@gmail.com>
2021-09-03 14:17:58 +0300

    add csv reader

---
+++ CodeTask.Lib/CsvReader.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CodeTask.Lib
+{
+    public class CsvReader
+    {
+        public IEnumerable<string> GetPropValues(string input)
+        {
+            if (!input.Contains('"'))
+            {
+                return input.Split(',');
+            }
+            else return GetPropValuesQuoted(input);
+        }
+
+        public IEnumerable<string> GetPropValuesQuoted(string input)
+        {
+            var headerLength = 8;
+            var propsProcessed = 0;
+            var curIdx = 0;
+            while(propsProcessed < headerLength)
+            {
+                if (input[curIdx] == '"')
+                {
+                    var propVal = GetPropValueQuoted(input.Substring(curIdx));
+                    curIdx += propVal.Length;
+                    yield return propVal;
+                }
+
+                var propValue = GetPropValue(input.Substring(curIdx));
+
+                curIdx += propValue.Length;
+                    
+                var isLastProp = curIdx == input.Length;
+                if (isLastProp)
+                {
+                    yield break;
+                }
+
+                yield return propValue;
+
+                curIdx++;
+            }
+        }
+
+        private string GetPropValue(string input)
+        {
+            var commaIdx = input.IndexOf(',');
+            var endsWithComma = commaIdx >= 0;
+
+            return endsWithComma
+                ? input.Substring(0, commaIdx)
+                : input;
+        }
+
+        private string GetPropValueQuoted(string input)
+        {
+            var currIdx = 1;
+
+            while (true)
+            {
+                currIdx = input.IndexOf('"', currIdx);
+
+                var isNotQuoteBefore = input[currIdx - 1] != '"';
+                var hasNextSymbol = currIdx + 1 != input.Length;
+                var isNotQuoteAfter = !hasNextSymbol || input[currIdx + 1] != '"';
+
+                if(isNotQuoteBefore && isNotQuoteAfter)
+                {
+                    break;
+                }
+            }
+
+            return input.Substring(currIdx);
+        }
+    }
+}
---
+++ CodeTask.Tests/CsvTest.cs
@@ -0,0 +1,42 @@
+﻿using CodeTask.Lib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CodeTask.Tests
+{
+    public class CsvTest
+    {
+        const string simpleData = "101,b603c59e-34a1-4754-aef8-baeed09466c4,Melanie,Yate,Melanie.Yate@yopmail.com,doctor,BJ,\"YHAoOEc\"\"YQIFIcOeNoqTqynlmEOgQXpLiiDW aZnj\"";
+        const string dataWithComma = "107,f3328b96-a0ea-4231-ac22-4a5b3b99a0f0,Fanny,Burch,Fanny.Burch@yopmail.com,worker,BJ,\"YwrqUZTxatTcErR ZA\"\"XkIYT ,lKJchmxn s\"";
+
+
+        [Theory]
+        [InlineData(simpleData, new string[] { 
+            "101",
+            "b603c59e-34a1-4754-aef8-baeed09466c4",
+            "Melanie",
+            "Yate",
+            "Melanie.Yate@yopmail.com",
+            "doctor",
+            "BJ",
+            "\"YHAoOEc\"\"YQIFIcOeNoqTqynlmEOgQXpLiiDW aZnj\""
+        })]
+        [InlineData(dataWithComma, new string[] {
+            "107",
+            "f3328b96-a0ea-4231-ac22-4a5b3b99a0f0",
+            "Fanny",
+            "Burch",
+            "Fanny.Burch@yopmail.com",
+            "worker",
+            "BJ",
+            "\"YwrqUZTxatTcErR ZA\"\"XkIYT ,lKJchmxn s\""
+        })]
+        public void EnsureData(string input, string[] output)
+        {
+            var reader = new CsvReader();
+            Assert.Equal(output, reader.GetPropValuesQuoted(input));
+        }
+    }
+}
